5b5d2531db44b53eed68b84bee9c078d9ef7cc56
Ismael Bonato <ismaelbonato@gmail.com>
2023-04-27 22:13:59 -0300

    added 09, 10 from chapter 2

---
+++ Chapter_02/Exercise_09/Ex10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// string global = empty
+std::string global_str;
+// global_int = 0
+int global_int;
+int main()
+{
+    // local_int = undefined
+    int local_int;
+    // local_str = empty
+    std::string local_str;
+
+
+    return 0;
+}
